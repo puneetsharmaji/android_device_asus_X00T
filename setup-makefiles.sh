PixelExperience-Devices/device_asus_X00TD

X00T: fix commit eba5e3f9 on setup-makefiles
well... my bad

Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

setup-makefiles.sh
@@ -26,24 +26,24 @@ INITIAL_COPYRIGHT_YEAR=2018
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "{$MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="{$MY_DIR}"/../../..
+LINEAGE_ROOT="${MY_DIR}/../../.."
 
-HELPER="{$LINEAGE_ROOT}"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "{$HELPER}" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "{$HELPER}"
+source "${HELPER}"
 
 # Initialize the helper
-setup_vendor "{$DEVICE}" "{$VENDOR}" "{$LINEAGE_ROOT}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}"
 
 # Copyright headers and guards
 write_headers
 
-write_makefiles "{$MY_DIR}"/proprietary-files.txt true
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
 # Finish
 write_footers

puneetsharmaji/android_device_asus_X00T

Signed-off-by: KapilTapsi <kapiltapsi@gmail.com>

setup-makefiles.sh
@@ -26,24 +26,24 @@ INITIAL_COPYRIGHT_YEAR=2018
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "{$MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="{$MY_DIR}"/../../..
+LINEAGE_ROOT="${MY_DIR}/../../.."
 
-HELPER="{$LINEAGE_ROOT}"/vendor/dot/build/tools/extract_utils.sh
-if [ ! -f "{$HELPER}" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${LINEAGE_ROOT}/vendor/dot/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "{$HELPER}"
+source "${HELPER}"
 
 # Initialize the helper
-setup_vendor "{$DEVICE}" "{$VENDOR}" "{$LINEAGE_ROOT}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}"
 
 # Copyright headers and guards
 write_headers
 
-write_makefiles "{$MY_DIR}"/proprietary-files.txt true
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
 # Finish
 write_footers